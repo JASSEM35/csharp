a828e9f1407bad98a0b23e3ce8c4087dca609562
JASSEM35 <jassem35@gmail.com>
2017-05-06 15:26:21 -0700

    Add project files.

---
+++ Test/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    class Class1
+    {
+        int Number;
+        string Name;
+        public Class1(string name)
+        {
+            Name = name;
+        }
+
+    public Class1(int number)
+        {
+            Number = number;    
+        }
+
+        public int numberMethod()
+         {
+            return Number;
+         }
+
+        public static void Jassem(string jassem)
+        {
+            MessageBox.Show(jassem);
+        }
+        public static void Jassem(int jassem)
+        {
+            MessageBox.Show(jassem.ToString());
+        }
+    }
+}
